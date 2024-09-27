18334ab41614d00da34fd2ae8a950ecc733d57e0
jooao7 <jvitorfjv781@gmail.com>
2024-09-27 15:08:10 -0300

    Initial commit

--- index.js
+++ index.js
@@ -23,5 +23,4 @@ const nomeHeroi = prompt("Digite o nome do herói:");
 const xpHeroi = parseInt(prompt("Digite a quantidade de XP do herói:"));
 
 const nivel = classificarHeroi(nomeHeroi, xpHeroi);
-
 console.log(`O herói de nome ${nomeHeroi} está no nível de ${nivel}`);
