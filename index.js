9e1b4e35631257cabf7f49a3f292874e9a572169
jooao7 <jvitorfjv781@gmail.com>
2024-09-27 14:59:14 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+function classificarHeroi(nome, xp) {
+  switch (true) {
+    case xp < 1000:
+      return 'Ferro';
+    case xp <= 2000:
+      return 'Bronze';
+    case xp <= 5000:
+      return 'Prata';
+    case xp <= 7000:
+      return 'Ouro';
+    case xp <= 8000:
+      return 'Platina';
+    case xp <= 9000:
+      return 'Ascendente';
+    case xp <= 10000:
+      return 'Imortal';
+    default:
+      return 'Radiante';
+  }
+}
+
+// Obtendo os dados do herói
+const nomeHeroi = prompt('Digite o nome do herói:');
+const xpHeroi = parseInt(prompt('Digite a quantidade de XP do herói:'));
+
+// Classificando o herói e exibindo o resultado
+const nivel = classificarHeroi(nomeHeroi, xpHeroi);
+console.log(`O herói de nome ${nomeHeroi} está no nível de ${nivel}`);
